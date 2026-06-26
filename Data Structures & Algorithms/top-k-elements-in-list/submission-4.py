2d9cef4b2c01d06f8fe39559e419c93e7fe0e328
Pedro Henrique <38144264+peedrohj@users.noreply.github.com>
2026-06-25 23:19:14 -0300

    Add: top-k-elements-in-list - submission-4

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-4.py
@@ -0,0 +1,37 @@
+"""
+
+Given an array of number returns the most frequent elements
+
+Requirements:
+- Return the top "k" elements
+- The answer is always unique
+- Return the output in any order
+
+Input: nums = [1,2,2,3,3,3], k = 2
+Output: [2,3]
+
+---
+
+- Go trough the array
+- Store the value and a counter
+- Increment the counter each time a number appears
+    - Check if this has been counted before
+    - If not initialize the data if a default value of "1"
+
+HashMap: Search O(1)
+- Sort the hasmap using the counter as the key
+- Return the hasmap top "k" elements
+
+"""
+
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        hashmap = {}
+        
+        for number in nums:
+            hashmap[str(number)] = 1 + hashmap.get(str(number), 0)
+
+        sorted_hash = dict(sorted(hashmap.items(), key=lambda x: x[1], reverse=True))
+
+        return list(sorted_hash.keys())[0:k]
