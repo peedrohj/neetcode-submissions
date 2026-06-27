59f64fe7c66c3ea7187ce70e1e05818c9814a0f0
Pedro Henrique <38144264+peedrohj@users.noreply.github.com>
2026-06-27 14:34:09 -0300

    Add: max-water-container - submission-1

---
+++ Data Structures & Algorithms/max-water-container/submission-1.py
@@ -0,0 +1,40 @@
+"""
+
+Create an algorithm that can return the area a square from a
+array of heights with a given index and value. 
+Input: height = [1,7,2,5,4,7,3,6]
+Output: 36
+
+Requirements:
+- Choose any two bars to form a container
+- Amount of water = Area of the container
+- Return the maximum area
+
+Solution:
+- Use a two pointers Solution
+- For each index, calculate the area for every elemente in array 
+- Store the value of the max area
+
+
+
+"""
+
+
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        max_area = 0
+        start, end = 0, len(heights) - 1
+
+        while end > start:
+            length = min(heights[start], heights[end])
+            width = end-start 
+
+            max_area = max(length * width, max_area)
+
+            if heights[start] > heights[end]:
+                end -= 1
+            else:
+                start += 1
+
+
+        return max_area
